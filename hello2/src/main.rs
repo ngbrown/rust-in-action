560d46c1b0ae52d7f8ca5bc1aa182b5f467fe664
Nathan Brown <nathan@nkbrown.us>
2021-08-28 11:48:33 -0700

    Finished second program, hello2

--- hello2/src/main.rs
+++ hello2/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+fn greet_world() {
     println!("Hello, world!");
+    let southern_germany = "Grüß Gott!";
+    let japan = "ハロー・ワールド";
+
+    let regions = [southern_germany, japan];
+    for region in regions {
+        println!("{}", &region);
+    }
+}
+
+fn main() {
+    greet_world();
 }
