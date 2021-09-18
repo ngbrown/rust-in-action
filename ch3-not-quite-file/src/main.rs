3e7a9f5489dfd4a56da0db672f41db2faf20a5ec
Nathan Brown <nathan@nkbrown.us>
2021-09-18 11:29:38 -0700

    ch3 - not-quite-file listing 3.3

--- ch3-not-quite-file/src/main.rs
+++ ch3-not-quite-file/src/main.rs
@@ -14,24 +14,30 @@ fn close(f: &mut File) -> bool {
     true
 }
 
-#[allow(dead_code)]
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
-    unimplemented!()
+fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+
+    save_to.reserve(read_length);
+    save_to.append(&mut tmp);
+    read_length
 }
 
 fn main() {
-    let mut f1 = File {
-        name: String::from("f1.txt"),
-        data: Vec::new(),
+    let mut f2 = File {
+        name: String::from("2.txt"),
+        data: vec![114, 117, 115, 116, 33],
     };
-    open(&mut f1);
 
-    let f1_name = &f1.name;
-    let f1_length = &f1.data.len();
+    let mut buffer: Vec<u8> = vec![];
 
-    //read(&mut f1, &mut vec![]);
-    close(&mut f1);
+    open(&mut f2);
+    let f2_length = read(&f2, &mut buffer);
+    close(&mut f2);
 
-    println!("{:?}", f1);
-    println!("{} is {} bytes long", f1_name, f1_length);
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f2);
+    println!("{} is {} bytes long", &f2.name, f2_length);
+    println!("{}", text)
 }
