f6000f97b6fc09ad0b66941f9daf885ee555d13b
Nathan Brown <nathan@nkbrown.us>
2021-09-18 11:58:16 -0700

    ch3 - not-quite-file listing 3.4

--- ch3-not-quite-file/src/main.rs
+++ ch3-not-quite-file/src/main.rs
@@ -6,6 +6,15 @@ struct File {
     data: Vec<u8>,
 }
 
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+        }
+    }
+}
+
 fn open(f: &mut File) -> bool {
     true
 }
@@ -24,20 +33,18 @@ fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
 }
 
 fn main() {
-    let mut f2 = File {
-        name: String::from("2.txt"),
-        data: vec![114, 117, 115, 116, 33],
-    };
+    let mut f3 = File::new("f3.txt");
+    f3.data = vec![114, 117, 115, 116, 33];
 
     let mut buffer: Vec<u8> = vec![];
 
-    open(&mut f2);
-    let f2_length = read(&f2, &mut buffer);
-    close(&mut f2);
+    open(&mut f3);
+    let f2_length = read(&f3, &mut buffer);
+    close(&mut f3);
 
     let text = String::from_utf8_lossy(&buffer);
 
-    println!("{:?}", f2);
-    println!("{} is {} bytes long", &f2.name, f2_length);
+    println!("{:?}", f3);
+    println!("{} is {} bytes long", &f3.name, f2_length);
     println!("{}", text)
 }
