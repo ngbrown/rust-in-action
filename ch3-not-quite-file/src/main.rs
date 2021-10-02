79c9d04aa4195a7eb0858e9be82d453629335f02
Nathan Brown <nathan@nkbrown.us>
2021-10-02 12:59:41 -0700

    ch3 - not-quite-file listing 3.16

--- ch3-not-quite-file/src/main.rs
+++ ch3-not-quite-file/src/main.rs
@@ -6,19 +6,19 @@ fn one_in(denominator: u32) -> bool {
 }
 
 #[derive(Debug, PartialEq)]
-enum FileState {
+pub enum FileState {
     Open,
     Closed,
 }
 
 #[derive(Debug)]
-struct File {
-    name: String,
+pub struct File {
+    pub name: String,
     data: Vec<u8>,
-    state: FileState,
+    pub state: FileState,
 }
 
-trait Read {
+pub trait Read {
     fn read(self: &Self, save_to: &mut Vec<u8>) -> Result<usize, String>;
 }
 
@@ -32,7 +32,7 @@ impl Display for FileState {
 }
 
 impl File {
-    fn new(name: &str) -> File {
+    pub fn new(name: &str) -> File {
         File {
             name: String::from(name),
             data: Vec::new(),
@@ -40,7 +40,7 @@ impl File {
         }
     }
 
-    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+    pub fn new_with_data(name: &str, data: &Vec<u8>) -> File {
         let mut f = File::new(name);
         f.data = data.clone();
         f
@@ -68,7 +68,7 @@ impl Display for File {
     }
 }
 
-fn open(mut f: File) -> Result<File, String> {
+pub fn open(mut f: File) -> Result<File, String> {
     if one_in(10_000) {
         let err_msg = String::from("Permission denied");
         return Err(err_msg);
@@ -77,7 +77,7 @@ fn open(mut f: File) -> Result<File, String> {
     Ok(f)
 }
 
-fn close(mut f: File) -> Result<File, String> {
+pub fn close(mut f: File) -> Result<File, String> {
     if one_in(100_000) {
         let err_msg = String::from("Interrupted by signal!");
         return Err(err_msg);
