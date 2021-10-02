0559366f44e1ca588cca4e8763a73d2a03e2004c
Nathan Brown <nathan@nkbrown.us>
2021-10-02 15:07:34 -0700

    ch4 - check-sats listing 4.7

--- ch4-check-sats/src/main.rs
+++ ch4-check-sats/src/main.rs
@@ -10,8 +10,9 @@ enum StatusMessage {
     Ok,
 }
 
-fn check_status(sat_id: CubeSat) -> StatusMessage {
-    StatusMessage::Ok
+fn check_status(sat_id: CubeSat) -> CubeSat {
+    println!("{:?}: {:?}", sat_id, StatusMessage::Ok);
+    sat_id
 }
 
 fn main() {
@@ -19,14 +20,12 @@ fn main() {
     let sat_b = CubeSat { id: 1 };
     let sat_c = CubeSat { id: 2 };
 
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
-    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+    let sat_a = check_status(sat_a);
+    let sat_b = check_status(sat_b);
+    let sat_c = check_status(sat_c);
 
     // "waiting" ...
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
-    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+    let sat_a = check_status(sat_a);
+    let sat_b = check_status(sat_b);
+    let sat_c = check_status(sat_c);
 }
