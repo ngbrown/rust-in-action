09375a45050966ad50d8ce97525261c1ae847486
Nathan Brown <nathan@nkbrown.us>
2021-10-10 11:34:35 -0700

    ch5 - visualizing-f32 listing 5.8

--- ch5-visualizing-f32/src/main.rs
+++ ch5-visualizing-f32/src/main.rs
@@ -4,4 +4,10 @@ fn main() {
     let sign_bit = n_bits >> 31;
 
     println!("sign_bit: {}", sign_bit);
+
+    let exponent_ = n_bits >> 23;
+    let exponent_ = exponent_ & 0xff;
+    let exponent = (exponent_ as i32) - 127;
+
+    println!("exponent: {}", exponent);
 }
