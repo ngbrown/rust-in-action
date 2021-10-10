019f26b30fde23e01066a671197413432e1664c3
Nathan Brown <nathan@nkbrown.us>
2021-10-10 11:32:52 -0700

    ch5 - visualizing-f32 listing 5.7

--- ch5-visualizing-f32/src/main.rs
+++ ch5-visualizing-f32/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let n: f32 = 42.42;
+    let n_bits: u32 = n.to_bits();
+    let sign_bit = n_bits >> 31;
+
+    println!("sign_bit: {}", sign_bit);
 }
