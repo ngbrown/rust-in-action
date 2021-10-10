986ad2169516cb681fc3577c0d3bae12be101c84
Nathan Brown <nathan@nkbrown.us>
2021-10-10 11:45:18 -0700

    ch5 - visualizing-f32 listing 5.9

--- ch5-visualizing-f32/src/main.rs
+++ ch5-visualizing-f32/src/main.rs
@@ -10,4 +10,18 @@ fn main() {
     let exponent = (exponent_ as i32) - 127;
 
     println!("exponent: {}", exponent);
+
+    let mut mantissa: f32 = 1.0;
+
+    for i in 0..23 {
+        let mask = 1 << i;
+        let one_at_bit_i = n_bits & mask;
+        if one_at_bit_i != 0 {
+            let i_ = i as f32;
+            let weight = 2_f32.powf(i_ - 23.0);
+            mantissa += weight;
+        }
+    }
+
+    println!("mantissa: {}", mantissa);
 }
