00165339bd898d7a779e21162a50c88be9593dff
Nathan Brown <nathan@nkbrown.us>
2021-10-16 12:53:18 -0700

    ch6 - particles listing 6.9, full

--- ch6-particles/src/main.rs
+++ ch6-particles/src/main.rs
@@ -2,6 +2,30 @@ use graphics::math::{add, mul_scalar, Vec2d};
 use piston_window::{clear, rectangle, PistonWindow, WindowSettings};
 use rand::rngs::ThreadRng;
 use rand::{thread_rng, Rng};
+use std::alloc::{GlobalAlloc, Layout, System};
+use std::time::Instant;
+
+#[global_allocator]
+static ALLOCATOR: ReportingAllocator = ReportingAllocator;
+
+struct ReportingAllocator;
+
+unsafe impl GlobalAlloc for ReportingAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let start = Instant::now();
+        let ptr = System.alloc(layout);
+        let end = Instant::now();
+        let time_taken = end - start;
+        let bytes_requested = layout.size();
+
+        eprintln!("{}\t{}", bytes_requested, time_taken.as_nanos());
+        ptr
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        System.dealloc(ptr, layout);
+    }
+}
 
 struct World {
     current_turn: u64,
