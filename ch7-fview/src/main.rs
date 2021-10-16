1321026faab704b116e5a8ef5e2c1fce701aa116
Nathan Brown <nathan@nkbrown.us>
2021-10-16 15:07:46 -0700

    ch7 - fview listing 7.4

--- ch7-fview/src/main.rs
+++ ch7-fview/src/main.rs
@@ -1,23 +1,26 @@
+use std::env;
+use std::fs::File;
 use std::io::Read;
 
 const BYTES_PER_LINE: usize = 16;
-const INPUT: &'static [u8] = br#"
-fn main() {
-    println!("Hello, world!");
-}"#;
 
 fn main() -> std::io::Result<()> {
-    let mut buffer: Vec<u8> = vec![];
-    INPUT.read_to_end(&mut buffer)?;
+    let arg1 = env::args().nth(1);
 
-    let mut position_in_input = 0;
-    for line in buffer.chunks(BYTES_PER_LINE) {
-        print!("[0x{:08x}] ", position_in_input);
-        for byte in line {
+    let fname = arg1.expect("usage: fview FILENAME");
+
+    let mut f = File::open(&fname).expect("Unable to open file.");
+    let mut pos = 0;
+    let mut buffer = [0; BYTES_PER_LINE];
+
+    while let Ok(_) = f.read_exact(&mut buffer) {
+        print!("[0x{:08x}] ", pos);
+        for byte in &buffer {
             print!("{:02x} ", byte);
         }
+
         println!();
-        position_in_input += BYTES_PER_LINE;
+        pos += BYTES_PER_LINE;
     }
 
     Ok(())
