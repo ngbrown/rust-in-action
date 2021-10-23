57c3687eea5a15e68ba097edb1fb3226e8d1cd51
Nathan Brown <nathan@nkbrown.us>
2021-10-23 16:07:42 -0700

    ch7 - actionkv listing 7.6, 7.8

--- ch7-actionkv/src/lib.rs
+++ ch7-actionkv/src/lib.rs
@@ -1,7 +1,35 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::io;
+use std::path::Path;
+
+type ByteString = Vec<u8>;
+type ByteStr = [u8];
+
+pub struct ActionKV {}
+
+impl ActionKV {
+    pub fn open(path: &Path) -> io::Result<Self> {
+        unimplemented!();
+    }
+
+    pub fn load(&mut self) -> io::Result<()> {
+        unimplemented!();
+    }
+
+    pub fn get(&mut self, key: &ByteStr) -> io::Result<Option<ByteString>> {
+        unimplemented!();
+    }
+
+    pub fn insert(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<()> {
+        unimplemented!();
+    }
+
+    #[inline]
+    pub fn update(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<()> {
+        unimplemented!();
+    }
+
+    #[inline]
+    pub fn delete(&mut self, key: &ByteStr) -> io::Result<()> {
+        unimplemented!();
     }
 }
