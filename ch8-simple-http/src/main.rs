f763fab7fb3f2c6fae9711b3d21cd4edea5e6f5b
Nathan Brown <nathan@nkbrown.us>
2021-10-31 12:51:06 -0700

    ch8-simple-http listing 8.2

--- ch8-simple-http/src/main.rs
+++ ch8-simple-http/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    //let url = "http://www.rustinaction.com/";
+    let url = "http://localhost:5000/";
+    let mut response = reqwest::get(url)?;
+
+    let content = response.text()?;
+    println!("{}", content);
+
+    Ok(())
 }
