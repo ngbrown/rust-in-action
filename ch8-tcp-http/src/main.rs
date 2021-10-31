e675341cea92805c5af2d6940d956d7ed1a112ff
Nathan Brown <nathan@nkbrown.us>
2021-10-31 14:36:30 -0700

    ch8-tcp-http listing 8.6

--- ch8-tcp-http/src/main.rs
+++ ch8-tcp-http/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Write;
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    //let host = "www.rustinaction.com:80";
+    let host = "localhost:5000";
+
+    let mut conn = TcpStream::connect(host)?;
+
+    conn.write_all(b"GET / HTTP/1.0")?;
+    conn.write_all(b"\r\n")?;
+
+    //conn.write_all(b"Host: www.rustinaction.com")?;
+    conn.write_all(b"Host: localhost")?;
+    conn.write_all(b"\r\n\r\n")?;
+
+    std::io::copy(&mut conn, &mut std::io::stdout())?;
+
+    Ok(())
 }
