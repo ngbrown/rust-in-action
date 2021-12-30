c7f1f4007bf7b32af6eef30d7c9f04f7135d0621
Nathan Brown <nathan@nkbrown.us>
2021-12-30 13:13:56 -0700

    ch9-clock listing 9.9

--- ch9-clock/src/main.rs
+++ ch9-clock/src/main.rs
@@ -11,7 +11,46 @@ impl Clock {
 
     #[cfg(windows)]
     fn set<Tz: TimeZone>(t: DateTime<Tz>) {
-        unimplemented!()
+        use chrono::{Datelike, Timelike, Weekday};
+        use kernel32::SetSystemTime;
+        use winapi::{SYSTEMTIME, WORD};
+
+        let t = t.with_timezone(&Local);
+
+        let mut systime: SYSTEMTIME = unsafe { zeroed() };
+
+        let dow = match t.weekday() {
+            Weekday::Mon => 1,
+            Weekday::Tue => 2,
+            Weekday::Wed => 3,
+            Weekday::Thu => 4,
+            Weekday::Fri => 5,
+            Weekday::Sat => 6,
+            Weekday::Sun => 0,
+        };
+
+        let mut ns = t.nanosecond();
+        let mut leap = 0;
+        let is_leap_second = ns > 1_000_000_000;
+        if is_leap_second {
+            ns -= 1_000_000_000;
+            leap += 1;
+        }
+
+        systime.wYear = t.year() as WORD;
+        systime.wMonth = t.month() as WORD;
+        systime.wDayOfWeek = dow as WORD;
+        systime.wDay = t.day() as WORD;
+        systime.wHour = t.hour() as WORD;
+        systime.wMinute = t.minute() as WORD;
+        systime.wSecond = (leap + t.second()) as WORD;
+        systime.wMilliseconds = (ns / 1_000_000) as WORD;
+
+        let systime_ptr = &systime as *const SYSTEMTIME;
+
+        unsafe {
+            SetSystemTime(systime_ptr);
+        }
     }
 
     #[cfg(not(windows))]
