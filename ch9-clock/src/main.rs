cc23e23447f4b336e1ef98d7eef05594d54d11c0
Nathan Brown <nathan@nkbrown.us>
2022-01-01 14:24:39 -0700

    ch9-clock refactor

--- ch9-clock/src/main.rs
+++ ch9-clock/src/main.rs
@@ -1,77 +1,10 @@
+mod clock;
 mod ntp;
 
 use crate::ntp::check_time;
-use chrono::{DateTime, Local, TimeZone};
+use chrono::DateTime;
 use clap::{App, Arg};
-use std::mem::zeroed;
-
-struct Clock;
-
-impl Clock {
-    fn get() -> DateTime<Local> {
-        Local::now()
-    }
-
-    #[cfg(windows)]
-    fn set<Tz: TimeZone>(t: DateTime<Tz>) {
-        use chrono::{Datelike, Timelike, Weekday};
-        use kernel32::SetSystemTime;
-        use winapi::{SYSTEMTIME, WORD};
-
-        let t = t.with_timezone(&Local);
-
-        let mut systime: SYSTEMTIME = unsafe { zeroed() };
-
-        let dow = match t.weekday() {
-            Weekday::Mon => 1,
-            Weekday::Tue => 2,
-            Weekday::Wed => 3,
-            Weekday::Thu => 4,
-            Weekday::Fri => 5,
-            Weekday::Sat => 6,
-            Weekday::Sun => 0,
-        };
-
-        let mut ns = t.nanosecond();
-        let mut leap = 0;
-        let is_leap_second = ns > 1_000_000_000;
-        if is_leap_second {
-            ns -= 1_000_000_000;
-            leap += 1;
-        }
-
-        systime.wYear = t.year() as WORD;
-        systime.wMonth = t.month() as WORD;
-        systime.wDayOfWeek = dow as WORD;
-        systime.wDay = t.day() as WORD;
-        systime.wHour = t.hour() as WORD;
-        systime.wMinute = t.minute() as WORD;
-        systime.wSecond = (leap + t.second()) as WORD;
-        systime.wMilliseconds = (ns / 1_000_000) as WORD;
-
-        let systime_ptr = &systime as *const SYSTEMTIME;
-
-        unsafe {
-            SetSystemTime(systime_ptr);
-        }
-    }
-
-    #[cfg(not(windows))]
-    fn set<Tz: TimeZone>(t: DateTime<Tz>) {
-        use libc::{settimeofday, suseconds_t, time_t, timeval, timezone};
-
-        let t = t.with_timezone(&Local);
-        let mut u: timeval = unsafe { zeroed() };
-
-        u.tv_sec = t.timestamp() as time_t;
-        u.tv_usec = t.timestamp_subsec_micros() as suseconds_t;
-
-        unsafe {
-            let mock_tz: *const timezone = std::ptr::null();
-            settimeofday(&u as *const timeval, mock_tz);
-        }
-    }
-}
+use clock::Clock;
 
 fn main() {
     let app = App::new("clock")
