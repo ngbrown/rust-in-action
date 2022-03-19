5f5ac4965be832af36eb84c098765b27ef4df3e7
Nathan Brown <nathan@nkbrown.us>
2022-03-19 11:50:23 -0700

    ch12-toy-global listing 12.5, 12.6

--- ch-12-toy-global/src/main.rs
+++ ch-12-toy-global/src/main.rs
@@ -1,3 +1,17 @@
+use rand;
+
+static mut SHUT_DOWN: bool = false;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        unsafe {
+            SHUT_DOWN = rand::random();
+        }
+        print!(".");
+
+        if unsafe { SHUT_DOWN } {
+            break;
+        };
+    }
+    println!();
 }
